a4581769383029650d2bee06711f837b932048c8
HailonZhon <hailong@ecut.edu.cn>
2024-05-09 00:01:19 +0800

    优化前

--- app/ImageProcess/main.py
+++ app/ImageProcess/main.py
@@ -4,7 +4,6 @@ import os
 import random
 import uuid
 from pathlib import Path
-
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
@@ -14,20 +13,20 @@ import logging
 
 from src.image_processing.image_processor import ImageProcessor
 
-# Configure logging
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-# Get the logger for pika and set its level to WARNING to reduce output
+# 配置日志，添加 %(name)s 来显示 logger 的名称
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# 设置 pika 日志级别为 WARNING，减少输出
 logging.getLogger("pika").setLevel(logging.WARNING)
-logger = logging.getLogger(__name__)
+logging.getLogger("ImageProcessor").setLevel(logging.WARNING)
 
 app = FastAPI()
 
 # 允许的跨域源列表
 origins = [
-    "http://localhost:3000",  # 假设你的前端运行在3000端口
+    "http://localhost:3000",
     "https://localhost:3000",
     "http://localhost:8080",
-    "http://localhost:8000",  # 允许FastAPI的Swagger UI访问
+    "http://localhost:8000",
 ]
 
 app.add_middleware(
@@ -38,7 +37,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# 服务静态文件
+# 配置静态文件服务
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # 确保上传目录存在
@@ -46,39 +45,53 @@ UPLOAD_DIR = Path("data/uploaded_images")
 UPLOAD_DIR.mkdir(exist_ok=True)
 
 base_url = os.getenv("BASE_URL")
-# base_url = "https://exuwomf4pougcn-3000.proxy.runpod.net"
 image_processor = ImageProcessor(base_url)
-
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
+    # 使用 __name__ 来获取当前模块的 logger
+    logger = logging.getLogger(__name__)
     await websocket.accept()
     try:
         while True:
+            start_time = datetime.datetime.now()
             data = await websocket.receive_bytes()
-            # 生成文件名
             file_path = UPLOAD_DIR / f"image_{datetime.datetime.now().isoformat()}.png"
-            # 异步写入文件
+
             async with aio_open(file_path, 'wb') as f:
                 await f.write(data)
-            logger.info(f"Saved image to {file_path}")
+            logger.info(f"websocket_endpoint:图片已保存至 {file_path}")
+
+            # 测量文件写入耗时
+            file_write_time = datetime.datetime.now() - start_time
+            logger.info(f"websocket_endpoint:文件写入耗时 {file_write_time.total_seconds()} 秒")
 
             # 处理图片
-            processed_image = await process_image(file_path)
+            start_time = datetime.datetime.now()
+            processed_image = await process_image(file_path, logger)
+            total_processing_time = datetime.datetime.now() - start_time
+            logger.info(f"websocket_endpoint:总处理图片耗时 {total_processing_time.total_seconds()} 秒")
+
             await websocket.send_bytes(processed_image)
+
+            # 测量总处理耗时
+            total_processing_time = datetime.datetime.now() - start_time
+            logger.info(f"websocket_endpoint:总处理耗时 {total_processing_time.total_seconds()} 秒")
+
     except WebSocketDisconnect:
-        logger.info("Client disconnected.")
+        logger.info("websocket_endpoint:客户端已断开连接")
 
+async def process_image(file_path, logger):
+    start_time = datetime.datetime.now()
 
-async def process_image(file_path):
     # 上传图片到服务器
     image_name_server = image_processor.upload_image(file_path)
+    upload_time = datetime.datetime.now() - start_time
+    logger.info(f"process_image:图片上传耗时 {upload_time.total_seconds()} 秒")
 
-    # 准备prompt workflow
     with open(Path('static/workflow_api_huihua_2_1.json'), 'r') as file:
         prompt_workflow = json.load(file)
 
-    # 设置prompt workflow
     prompt_workflow["13"]["inputs"]["image"] = f"/workspace/ComfyUI/input/{image_name_server}"
     prompt_workflow["3"]["inputs"]["denoise"] = 1.0
     prompt_workflow["3"]["inputs"]["seed"] = random.randint(0, 4284967295) * random.randint(0, 10000)
@@ -86,37 +99,44 @@ async def process_image(file_path):
     prompt_workflow["29"]["inputs"]["width"] = 618
     prompt_workflow["29"]["inputs"]["height"] = 884
 
-    # 生成 UUID 并发送到队列
     uuid_value = str(uuid.uuid4())
-    rabbitmq_host = os.getenv('RABBITMQ_HOST', 'rabbitmq')  # 默认为localhost
-    # rabbitmq_host = "localhost"
-
-
+    rabbitmq_host = os.getenv('RABBITMQ_HOST', 'rabbitmq')
 
     try:
+        start_time = datetime.datetime.now()
         connection = pika.BlockingConnection(pika.ConnectionParameters(host=rabbitmq_host))
         channel = connection.channel()
         channel.queue_declare(queue='uuid_queue')
         channel.basic_publish(exchange='', routing_key='uuid_queue', body=uuid_value)
-        logger.info(f"Successfully published UUID {uuid_value} to RabbitMQ.")
+        logger.info(f"process_image:成功将 UUID {uuid_value} 发布到 RabbitMQ")
+
+        # 测量 RabbitMQ 消息发布耗时
+        rabbitmq_publish_time = datetime.datetime.now() - start_time
+        logger.info(f"process_image:RabbitMQ 消息发布耗时 {rabbitmq_publish_time.total_seconds()} 秒")
+
     except Exception as e:
-        logger.error(f"Failed to publish UUID to RabbitMQ: {e}")
+        logger.error(f"process_image:发布 UUID 到 RabbitMQ 失败: {e}")
         return None
     finally:
         if connection.is_open:
             connection.close()
-            logger.info("RabbitMQ connection closed.")
-    # 队列prompt
+            logger.info("process_image:RabbitMQ 连接已关闭")
+
+    start_time = datetime.datetime.now()
     prompt_id = image_processor.queue_prompt(prompt_workflow)
     if not prompt_id:
         return None
-    # 等待处理完成信号
+
+    # 测量队列操作耗时
+    queue_time = datetime.datetime.now() - start_time
+    logger.info(f"process_image:队列操作耗时 {queue_time.total_seconds()} 秒")
+
     image_processor.wait_for_image_processed_signal()
 
     images = image_processor.get_images()
     if images:
-        logger.info(f"Successfully processed images, returning the first image.")
+        logger.info(f"process_image:成功处理图片，返回第一张图片")
         return images[0]
     else:
-        logger.info("No images processed.")
+        logger.info("process_image:没有处理的图片")
         return None
--- app/ImageProcess/src/image_processing/image_processor.py
+++ app/ImageProcess/src/image_processing/image_processor.py
@@ -1,14 +1,17 @@
-import os.path
 import os
-
+import os.path
 import requests
 import json
 import pika
+import logging
+import time
 from requests_toolbelt.multipart.encoder import MultipartEncoder
 
+# 配置日志
 
 class ImageProcessor:
     def __init__(self, base_url, proxy_url=None):
+        self.logger = logging.getLogger('ImageProcessor')
         self.base_url = base_url
         self.proxies = {"http": proxy_url, "https": proxy_url} if proxy_url else None
         self.image_name_server = None
@@ -18,6 +21,7 @@ class ImageProcessor:
         url = f"{self.base_url}/upload/image"
         filename = image_path.name
         try:
+            start_time = time.time()
             with open(image_path, 'rb') as file:
                 m = MultipartEncoder(
                     fields={'image': (filename, file, 'image/png')}
@@ -25,21 +29,24 @@ class ImageProcessor:
                 response = requests.post(url, data=m, headers={'Content-Type': m.content_type}, proxies=self.proxies)
                 if response.status_code == 200:
                     self.image_name_server = response.json()['name']
-                    print('Image uploaded successfully', response.json())
+                    self.logger.info(f'upload_image:图片上传成功 {response.json()}')
                 else:
-                    print('Failed to upload image', response.status_code, response.text)
+                    logging.error(f'upload_image:图片上传失败 {response.status_code} {response.text}')
                     return None
+            upload_duration = time.time() - start_time
+            self.logger.info(f'upload_image:上传图片耗时 {upload_duration:.2f} 秒')
         except Exception as e:
-            print(f'Error: {e}')
+            logging.error(f'upload_image:上传图片过程中出错: {e}')
             return None
         return self.image_name_server
 
     def queue_prompt(self, prompt_workflow):
         if self.image_name_server is None:
-            print("请先上传一张照片和选择风格")
+            self.logger.info("queue_prompt:请先上传一张图片和选择风格")
             return None
 
         try:
+            start_time = time.time()
             request_data = json.dumps({"prompt": prompt_workflow})
             response = requests.post(
                 f"{self.base_url}/prompt",
@@ -49,63 +56,83 @@ class ImageProcessor:
             )
             if response.status_code == 200:
                 self.prompt_id = response.json()['prompt_id']
-                print(self.prompt_id)
+                self.logger.info(f'queue_prompt:Prompt ID: {self.prompt_id}')
+                queue_duration = time.time() - start_time
+                self.logger.info(f'queue_prompt:队列Prompt耗时 {queue_duration:.2f} 秒')
                 return self.prompt_id
             else:
-                print('Failed to make the API request')
+                logging.error('queue_prompt:API 请求失败')
                 return None
         except Exception as e:
-            print(f'Exception: {e}')
+            logging.error(f'queue_prompt:队列Prompt过程中出错: {e}')
             return None
 
     def get_images(self):
         if self.prompt_id is None:
-            print("No prompt ID available to fetch images.")
+            self.logger.info("get_images:没有可用的 Prompt ID 来获取图片。")
             return []
 
-        response = requests.get(f"{self.base_url}/history/{self.prompt_id}")
-        data = response.json()
-        if not data:
-            return []
-        images_data = []
-        outputs = data[self.prompt_id]['outputs']
+        try:
+            start_time = time.time()
+            response = requests.get(f"{self.base_url}/history/{self.prompt_id}")
+            data = response.json()
+            if not data:
+                return []
+            images_data = []
+            outputs = data[self.prompt_id]['outputs']
 
-        for node_id, node_output in outputs.items():
-            if 'images' in node_output:
-                for image in node_output['images']:
-                    image_data = self.get_server_image(image['filename'], image['subfolder'], image['type'])
-                    if image_data:
-                        images_data.append(image_data)
-        return images_data
+            for node_id, node_output in outputs.items():
+                if 'images' in node_output:
+                    for image in node_output['images']:
+                        image_data = self.get_server_image(image['filename'], image['subfolder'], image['type'])
+                        if image_data:
+                            images_data.append(image_data)
+            get_images_duration = time.time() - start_time
+            self.logger.info(f'get_images:获取图片耗时 {get_images_duration:.2f} 秒')
+            return images_data
+        except Exception as e:
+            logging.error(f'get_images:获取图片过程中出错: {e}')
+            return []
 
     def get_server_image(self, filename, subfolder, image_type):
         url = f"{self.base_url}/view?filename={filename}&subfolder={subfolder}&type={image_type}"
-        response = requests.get(url, stream=True)
-        if response.status_code == 200:
-            image_data = response.content
-            if not os.path.exists('data/output'):
-                os.makedirs('data/output')
-            with open(f"data/output/{filename}", "wb") as f:
-                f.write(image_data)
-            print("Image retrieved successfully")
-            return image_data
-        else:
-            print("Failed to load image")
+        try:
+            start_time = time.time()
+            response = requests.get(url, stream=True)
+            if response.status_code == 200:
+                image_data = response.content
+                if not os.path.exists('data/output'):
+                    os.makedirs('data/output')
+                with open(f"data/output/{filename}", "wb") as f:
+                    f.write(image_data)
+                self.logger.info("get_server_image:图片成功加载")
+                get_server_image_duration = time.time() - start_time
+                self.logger.info(f'get_server_image:加载服务器图片耗时 {get_server_image_duration:.2f} 秒')
+                return image_data
+            else:
+                logging.error("get_server_image:加载图片失败")
+                return None
+        except Exception as e:
+            logging.error(f'get_server_image:加载服务器图片过程中出错: {e}')
             return None
 
     def wait_for_image_processed_signal(self):
         # 等待 WebSocket 通信程序发送的处理完成信号
         rabbitmq_host = os.getenv('RABBITMQ_HOST', 'rabbitmq')  # 默认为localhost
-        # rabbitmq_host = "localhost"
-        connection = pika.BlockingConnection(pika.ConnectionParameters(host=rabbitmq_host))
-        channel = connection.channel()
-        channel.queue_declare(queue='image_processed')
+        try:
+            start_time = time.time()
+            connection = pika.BlockingConnection(pika.ConnectionParameters(host=rabbitmq_host))
+            channel = connection.channel()
+            channel.queue_declare(queue='image_processed')
 
-        def callback(ch, method, properties, body):
-            if body.decode() == 'done':
-                ch.stop_consuming()
+            def callback(ch, method, properties, body):
+                if body.decode() == 'done':
+                    ch.stop_consuming()
 
-        channel.basic_consume(queue='image_processed', on_message_callback=callback, auto_ack=True)
-        channel.start_consuming()
-        connection.close()
-
+            channel.basic_consume(queue='image_processed', on_message_callback=callback, auto_ack=True)
+            channel.start_consuming()
+            connection.close()
+            wait_duration = time.time() - start_time
+            self.logger.info(f'wait_for_image_processed_signal:等待图片处理信号耗时 {wait_duration:.2f} 秒')
+        except Exception as e:
+            logging.error(f'wait_for_image_processed_signal:等待处理信号过程中出错: {e}')
